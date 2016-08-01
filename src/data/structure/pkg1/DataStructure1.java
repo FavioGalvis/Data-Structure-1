dfa5eedf6100d596c581a825cfb91aec9801c533
Favio Arturo Galvis <favio.agg@gmail.com>
2016-07-31 20:28:09 -0500

    Listas Enlazadas
    Clase del 2 de Julio

---
+++ src/data/structure/pkg1/DataStructure1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data.structure.pkg1;
+
+/**
+ *
+ * @author 201521268405
+ */
+public class DataStructure1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void a(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
