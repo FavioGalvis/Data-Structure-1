db04acb8bb34a0a317162db3aa18ee1e58b4d03e
Favio Galvis <favio.agg@gmail.com>
2016-09-08 16:57:03 -0500

    Primer Parcial

--- src/Listas.java
+++ src/Listas.java
@@ -3,91 +3,12 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Listas {
- private Nodo L;//inicio de la lista
+ private Nodo L;//inicio de la lista de nodos
  public Listas()
  {
      L=null; //lista vacia
  }       
-//-------------imprimir
- public void Imprimir()
- {
-   String cad;
-   cad="===============================\n";
-   cad+="=======Lista de Estudiantes====\n";
-   cad+="===============================\n";
-   Nodo p=L;
-   if(L!=null){
-    do{
-     cad+=p.id+" - "+p.nombre+" - "+p.genero+" - "+p.programa+" - "+p.valor+"\n";   
-     p=p.sig;
-    }while(p!=L);
-   }
-   cad+="===============================\n";
-   javax.swing.JOptionPane.showMessageDialog(null,cad);
- }
-//-------------imprimir inverso
- public void Imprimir_inverso()
- {
-   String cad;
-   cad="===============================\n";
-   cad+="=======Datos de la lista======\n";
-   cad+="===============================\n";
-   Nodo p=Ultimo();
-   if(L!=null){
-    do{
-     cad+=p.id+" - "+p.nombre+" - "+p.genero+" - "+p.programa+" - "+p.valor+"\n";   
-     p=p.ant;
-    }while(p!=L.ant);
-   }
-   cad+="===============================\n";
-   javax.swing.JOptionPane.showMessageDialog(null,cad);
- }
-//---- ultimo
- public Nodo Ultimo()
- {
-     Nodo p=L;
-     if(L!=null){
-        p=p.ant;
-        return p;
-     }
-     return null;
- }//end Ultimo        
-//------buscar
- public Nodo Buscar(int dato)
- {
-     Nodo p=L;
-     if(L!=null){
-        do{
-         if(p.id==dato){
-             return p;
-         }   
-         p=p.sig;
-        }while(p!=L);
-     }
-     return null;
- }//end Buscar 
-//--------elementos posiciones pares
- public void Pares()
- {
-   String cad;
-   cad="===============================\n";
-   cad+="=======Datos en pos pares======\n";
-   cad+="===============================\n";
-   Nodo p=L;
-   int c=1;
-   if(L!=null){
-    do{
-     if(c % 2 == 0){
-     cad+=p.id+" - "+p.nombre+" - "+p.genero+" - "+p.programa+" - "+p.valor+"\n";
-     }
-     p=p.sig;
-     c++;
-    }while(p!=L);
-   }   
-   cad+="===============================\n";
-   javax.swing.JOptionPane.showMessageDialog(null,cad);
- }      
-//--------insertar al inicio
+//--------1. Insertar al inicio
  public void Ins_inicio(Nodo data)
  {
      Nodo x= new Nodo(data);
@@ -98,8 +19,9 @@ public class Listas {
          L.ant=x;
      }
      L=x;
- }        
-//--------insertar al final
+ }
+ 
+//--------2. Insertar al final
  public void Ins_final(Nodo data)
  {
      Nodo x= new Nodo(data);
@@ -113,26 +35,8 @@ public class Listas {
         L=x;
      }
  }
- //-----insertar despues de
- public void Ins_despues_de (int dato1, Nodo data){
-     Nodo b=Buscar(dato1);
-     Nodo x;
-     if(b!=null){
-        if(b.sig==L){
-            Ins_final(data);
-        } else {
-            x= new Nodo(data);
-            x.sig=b.sig;
-            x.ant=b;
-            (b.sig).ant=x;
-            b.sig=x;
-        }
-     }else{
-         javax.swing.JOptionPane.showMessageDialog(null,"Nodo no encontrado.");
-     }
- }
- //-----insertar antes de
- public void Ins_antes_de (int dato1, Nodo data){
+//-----3. Insertar antes de
+ public boolean Ins_antes_de (int dato1, Nodo data){
      Nodo b= Buscar(dato1);
      Nodo x;
      if(b!=null){
@@ -145,12 +49,51 @@ public class Listas {
              (b.ant).sig=x;
              b.ant=x;
          }
+         return true;
      }else{
-         javax.swing.JOptionPane.showMessageDialog(null,"Nodo no encontrado.");
+         javax.swing.JOptionPane.showMessageDialog(null,"Usuario no encontrado.");
+         return false;
      }
  }
- //-----Eliminar un nodo
- public void Eliminar(int dato1){
+ 
+//-----4. Insertar despues de
+ public boolean Ins_despues_de (int dato1, Nodo data){
+     Nodo b=Buscar(dato1);
+     Nodo x;
+     if(b!=null){
+        if(b.sig==L){
+            Ins_final(data);
+        } else {
+            x= new Nodo(data);
+            x.sig=b.sig;
+            x.ant=b;
+            (b.sig).ant=x;
+            b.sig=x;
+        }
+        return true;
+     }else{
+         javax.swing.JOptionPane.showMessageDialog(null,"Usuario no encontrado.");
+         return false;
+     }
+ }
+ 
+ //------5. Buscar
+ public Nodo Buscar(int dato)
+ {
+     Nodo p=L;
+     if(L!=null){
+        do{
+         if(p.id==dato){
+             return p;
+         }   
+         p=p.sig;
+        }while(p!=L);
+     }
+     return null;
+ }
+ 
+ //-----6. Eliminar un nodo
+ public boolean Eliminar(int dato1){
      Nodo b= Buscar(dato1);
      if(b!=null){
         if(L.sig==L){
@@ -173,58 +116,34 @@ public class Listas {
         b.ant=null;
         b.sig=null;
         b=null;
+        return true;
      }else{
-         javax.swing.JOptionPane.showMessageDialog(null,"Nodo no encontrado.");
+         javax.swing.JOptionPane.showMessageDialog(null,"Usuario no encontrado.");
+         return false;
      }
  }
- //-----insertar N aleatorios
- public void Ins_aleatorios(int dato)
+ 
+ //----- Aux.) Imprime todos los nodos
+ public void Imprimir()
  {
-    int limite=9999;
-    Random rand = new Random();
-    for(int i=0;i<=dato-1;i++){
-     Nodo x= new Nodo();
-     x.id=rand.nextInt(limite);
-     x.valor=rand.nextInt(limite);
-     x.nombre="Nom-no-def"+rand.nextInt(limite);
-     x.genero="Gen-no-def"+rand.nextInt(limite);
-     x.programa="Pro-no-def"+rand.nextInt(limite);
-     if(L!=null) {
-         Ins_final(x);
-     }
-     else {
-         L=x;
-     }
-    }
- }
- //----mostrar menor valor
- public void Buscar_menor_valor(){
    String cad;
-   Nodo x = new Nodo();
-   cad="===============================\n";
-   cad+="=======Menor valor de la lista======\n";
-   cad+="===============================\n";
+   int bonoanual=0;
+   cad="=======Lista de Usuarios====\n";
+   cad+="============================\n";
    Nodo p=L;
    if(L!=null){
-    x.valor=p.valor;
     do{
-     if(x.valor>p.valor){
-         x=p;
-     }
+     bonoanual=(int) ((p.tservicio/12)*(p.salario*0.01));
+     cad+=p.id+" - "+p.nombre+" - "+p.genero+" - "+p.ecivil+" - "+p.tservicio+" - $"+p.salario+" - $"+bonoanual+"\n";   
      p=p.sig;
     }while(p!=L);
    }
-   cad+=x.id+" - "+x.nombre+" - "+x.genero+" - "+x.programa+" - "+x.valor+"\n";
    cad+="===============================\n";
    javax.swing.JOptionPane.showMessageDialog(null,cad);
  }
- //----------- Contar nodos-----------
- public void Contar_nodos(){
-   String cad;
+ 
+ public int Contar_nodos(){
    int cant = 0;
-   cad="===============================\n";
-   cad+="=Cuenta de Nodos de la lista==\n";
-   cad+="===============================\n";
    Nodo p=L;
    if(L!=null){
     do{
@@ -232,9 +151,78 @@ public class Listas {
      p=p.sig;
     }while(p!=L);
    }
-   cad+=cant+"\n";
-   cad+="===============================\n";
-   javax.swing.JOptionPane.showMessageDialog(null,cad);
+   return cant;
+ }
+
+ public void Ordenar_Nodos(){
+     Nodo temp = new Nodo();
+     Nodo p;
+     Nodo q;
+     p=L;
+     if(L!=null){
+         do{
+             q=L;
+             do{
+                 if(q.id>p.id){
+                     temp.id=p.id;
+                     temp.nombre=p.nombre;
+                     temp.ecivil=p.ecivil;
+                     temp.genero=p.genero;
+                     temp.salario=p.salario;
+                     temp.tservicio=p.tservicio;
+                     p.id=q.id;
+                     p.nombre=q.nombre;
+                     p.ecivil=q.ecivil;
+                     p.genero=q.genero;
+                     p.salario=q.salario;
+                     p.tservicio=q.tservicio;
+                     q.id=temp.id;
+                     q.nombre=temp.nombre;
+                     q.ecivil=temp.ecivil;
+                     q.genero=temp.genero;
+                     q.salario=temp.salario;
+                     q.tservicio=temp.tservicio;        
+                 }
+                 q=q.sig;
+             }while(q!=L);
+             p=p.sig;
+         }while(p!=L);
+
+         }else{
+         javax.swing.JOptionPane.showMessageDialog(null,"Usuario no encontrado.");
+     }
+         
+}
+ 
+ //-----6. Eliminar un nodo
+ public boolean Eliminar(Nodo x){
+     Nodo b= x;
+     if(b!=null){
+        if(L.sig==L){
+            L=null;
+        }else{
+            if(b==L){
+                (L.sig).ant=L.ant;
+                (L.ant).sig=L.sig;
+                L=L.sig;
+            } else {
+                if(b==L.ant){
+                    L.ant=(L.ant).ant;
+                    (L.ant).sig=L;
+                } else {
+                    (b.ant).sig=b.sig;
+                    (b.sig).ant=b.ant;
+                }
+            }
+        }
+        b.ant=null;
+        b.sig=null;
+        b=null;
+        return true;
+     }else{
+         javax.swing.JOptionPane.showMessageDialog(null,"Usuario no encontrado.");
+         return false;
+     }
  }
  
 
--- src/Nodo.java
+++ src/Nodo.java
@@ -1,23 +1,25 @@
 public class Nodo {
- public int id;
- public String nombre;
- public String genero;
- public String programa;
- public double valor;
- public Nodo sig;//liga
- public Nodo ant;//liga
- public Nodo()
+ public int id;//identificacion del usuario
+ public String nombre;//nombres del usuario
+ public String genero;//genero del usuario
+ public String ecivil;//estado civil del usuario
+ public double salario;//salario del usuario
+ public int tservicio;//tiempo de servicio en meses
+ public Nodo sig;//apuntador a nodo siguiente
+ public Nodo ant;//apuntador a nodo anterior
+ public Nodo()//constructor nulo
  {
      sig=this;
      ant=this;
  }       
- public Nodo(Nodo data)
+ public Nodo(Nodo data)//constructor sobrecargado con datos
  {
      id=data.id;
      nombre=data.nombre;
      genero=data.genero;
-     programa=data.programa;
-     valor=data.valor;
+     ecivil=data.ecivil;
+     salario=data.salario;
+     tservicio=data.tservicio;
      sig=this;
      ant=this;
  }
